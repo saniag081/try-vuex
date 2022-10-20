942c6de484f5d460016a9905107a764676c186c4
Santiago G <sgonzalez@modyo.com>
2022-10-19 19:04:19 -0500

    feat(store): getters

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,16 @@ import { createStore } from 'vuex';
 const store = createStore({
   state() {
     return {
-      username: 'Santiago',
+      username: 'Santiago.prueba',
     };
+  },
+  getters: {
+    firstName: (state) => (caracter) => {
+      return state.username.split('').reverse().join(caracter);
+    },
+    // firstName(state) {
+    //   return state.username.split('.')[0];
+    // },
   },
 });
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { RouterView, RouterLink } from 'vue-router'
 import InputSearch from '@/components/InputSearch.vue'
 import ProfileCard from '@/components/ProfileCard.vue'
 import ChatItem from '@/components/ChatItem.vue'
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   components: {
@@ -34,6 +34,7 @@ export default {
   computed: {
     foo() {},
     ...mapState(['username']),
+    ...mapGetters(['firstName']),
   }
 }
 </script>
@@ -44,7 +45,7 @@ export default {
     <InputSearch v-model="search" />
     <ProfileCard
       :avatar="profile.avatar"
-      :username="username"
+      :username="firstName('-')"
       :status="profile.status"
     />
     <RouterLink to="/" class="channels-title">Canales <Icon icon="carbon:hashtag" /></RouterLink>
