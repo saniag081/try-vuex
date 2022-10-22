7feb2fd610f394864b8dae5995b343ea70fc0b60
Santiago G <sgonzalez@modyo.com>
2022-10-21 20:50:18 -0500

    feat: added message module

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { createStore } from 'vuex';
 import { COMMIT_SET_STATUS } from '@/common/mutations-types.js';
 import profile from '@/store/modules/profile';
 import channels  from '@/store/modules/channels';
+import messages from '@/store/modules/message';
 
 const store = createStore({
   state() {
@@ -17,6 +18,7 @@ const store = createStore({
   modules: {
     profile,
     channels,
+    messages,
   },
 });
 
